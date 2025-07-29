4d18f1cfef2afa9082f71a924f64d84ad16da3f8
caulesti <caulesti@espol.edu.ec>
2025-07-29 15:16:29 -0500

    requerimiento 4

--- react-course/src/Pages/App/index.jsx
+++ react-course/src/Pages/App/index.jsx
@@ -1,3 +1,5 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom'
+
 import Home from '../Home'
 import MyAccount from '../MyAccount'
 import MyOrder from '../MyOrder'
@@ -7,17 +9,23 @@ import SignIn from '../SignIn'
 
 import './App.css'
 
-function App() {
+const AppRoutes = () => {
+  let routes = useRoutes([ 
+    { path: '/', element: <Home /> },
+    { path: '/my_account', element: <MyAccount /> },
+    { path: '/my_order', element: <MyOrder /> },
+    { path: '/my_orders', element: <MyOrders /> },
+    { path: '/sign_in', element: <SignIn /> },
+    { path: '/*', element: <NotFound /> },
+  ])
+  return routes
+}
 
+const App = () => {
   return (
-    <div className='bg-red-100'>
-      <Home />
-      <MyAccount />
-      <MyOrder />
-      <MyOrders />
-      <NotFound />
-      <SignIn />
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 
