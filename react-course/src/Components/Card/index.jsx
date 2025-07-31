8a50c6b8a4b241f29b70afd94d2b2f0e8ddb60aa
caulesti <caulesti@espol.edu.ec>
2025-07-31 13:50:34 -0500

    Requerimiento 16

--- react-course/src/Components/Card/index.jsx
+++ react-course/src/Components/Card/index.jsx
@@ -10,9 +10,11 @@ const Card = (data) => {
         context.openProductDetail()
         context.setProductToShow(productDetail)
     }
-    const addProductsToCart = (productData) => {
+    const addProductsToCart = (event, productData) => {
+        event.stopPropagation()
         context.setCartProducts([...context.cartProducts, productData])
         context.setCount(context.count + 1)
+        context.openCheckoutSideMenu()
         console.log('cart: ', context.cartProducts)
     }
     return (
@@ -24,7 +26,7 @@ const Card = (data) => {
                 <img className='w-full h-full object-cover rounded-lg' src={data.data.images} alt={data.data.title} />
                 <div 
                     className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'
-                    onClick={() => addProductsToCart(data.data)}>
+                    onClick={(event) => addProductsToCart(event, data.data)}>
                     <PlusIcon className='h-6 w-6 text-black'></PlusIcon>
                 </div>
             </figure>                
