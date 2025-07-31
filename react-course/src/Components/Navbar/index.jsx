8e88c2b1c97f5785c8baf2fcc93e00efd1d93b2e
caulesti <caulesti@espol.edu.ec>
2025-07-31 09:30:34 -0500

    requerimiento 12

--- react-course/src/Components/Navbar/index.jsx
+++ react-course/src/Components/Navbar/index.jsx
@@ -1,5 +1,8 @@
 import { NavLink } from 'react-router-dom'
 import { useContext } from 'react'
+
+import { ShoppingBagIcon } from '@heroicons/react/24/solid'
+
 import { ShoppingCartContext } from '../../Context'
 
 const Navbar = () => {
@@ -100,8 +103,11 @@ const Navbar = () => {
                         Sign In
                     </NavLink>
                 </li>
-                <li>
-                    🛒 { context.count }
+                <li className='flex items-center'>
+                    <ShoppingBagIcon className='h-6 w-6 text-black'></ShoppingBagIcon> 
+                    <div>
+                        { context.count }
+                    </div>
                 </li>
             </ul>
         </nav>
