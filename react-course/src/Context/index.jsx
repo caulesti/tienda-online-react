779cbe70e61c20d84dad7a7ca665cecb514ce78e
caulesti <caulesti@espol.edu.ec>
2025-08-01 15:44:42 -0500

    requerimiento 28 update

--- react-course/src/Context/index.jsx
+++ react-course/src/Context/index.jsx
@@ -3,14 +3,6 @@ import { createContext, useState, useEffect } from 'react'
 export const ShoppingCartContext = createContext()
 
 export const ShoppingCartProvider = ({children}) => {
-    //Get products
-    const [items, setItems] = useState(null) 
-
-    useEffect(() => {
-        fetch('https://api.escuelajs.co/api/v1/products')
-        .then(response => response.json())
-        .then(data => setItems(data))
-    }, [])
 
     // Shopping Cart - Increment quantity
     const [count, setCount] = useState(0)
@@ -35,8 +27,31 @@ export const ShoppingCartProvider = ({children}) => {
     const [order, setOrder] = useState([])
 
     // Get products by title
-    const [searchByTitle, setSearchByTitle] = useState([])
-    console.log('searchByTitle: ', searchByTitle)
+    const [searchByTitle, setSearchByTitle] = useState(null)
+
+    //Get products
+    const [items, setItems] = useState(null) 
+
+    //Get products
+    const [filteredItems, setFilteredItems] = useState(null) 
+
+    useEffect(() => {
+        fetch('https://api.escuelajs.co/api/v1/products')
+        .then(response => response.json())
+        .then(data => setItems(data))
+    }, [])
+
+    const filteredItemsByTitle = (items, searchByTitle) => {
+        return items?.filter(item => item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
+    }
+
+    useEffect(() => {
+        if (searchByTitle) {
+            setFilteredItems(filteredItemsByTitle(items, searchByTitle))
+        }
+    }, [items, searchByTitle])
+
+    console.log('filteredItems: ', filteredItems)
 
     return (
         <ShoppingCartContext.Provider value={{
